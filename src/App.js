3c86867ee80b8123a7c3d5b31d6835125123ccdb
Shakshi <sakshi.tomar@wayyourchild.in>
2025-02-18 12:17:17 +0530

    resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
 import Services from './components/Services';
 import LatestProjects from './components/LatestProjects';
 import LatestThinking from './components/LatestThinking';
@@ -9,21 +7,11 @@ import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 import LandingPage from './components/Navbar';
 import CollaborationProcess from './components/CollabrationProcess';
-// import Hero from './components/Hero';
-// import ClientLogos from './components/ClientLogos';
-// import Testimonials from './components/Testimonials';
-// import Services from './components/Services';
-// import Projects from './components/Projects';
-// import Process from './components/Process';
-// import LatestThinking from './components/LatestThinking';
-// import Contact from './components/Contact';
-// import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Navbar /> */}
         <LandingPage />
         <Services />
         <LatestProjects/>
@@ -31,15 +19,7 @@ function App() {
         <LatestThinking />
         <ContactForm />
         <Footer/>
-        {/* <Hero />
-        <ClientLogos />
-        <Testimonials />
-        <Services />
-        <Projects />
-        <Process />
-        <LatestThinking />
-        <Contact />
-        <Footer /> */}
+       
       </div>
     </Router>
   );
